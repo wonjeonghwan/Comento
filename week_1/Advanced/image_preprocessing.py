0158df9b690b5ce02ed35aff0d8362511dbce08d
wonjeonghwan <wonjeonghwan@naver.com>
2025-08-22 15:04:11 +0900

    Refactor : is image too small

--- week_1/Advanced/image_preprocessing.py
+++ week_1/Advanced/image_preprocessing.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from typing import Tuple
+import cv2
 import random
 import numpy as np
 from datasets import load_dataset
@@ -13,13 +14,12 @@ SAVE_DIR.mkdir(parents=True, exist_ok=True)
 TARGET_SIZE: Tuple[int, int] = (224, 224)
 GAUSSIAN_BLUR_RADIUS: float = 0.2       # 가우시안 블러 값
 DARK_MEAN_THRESHOLD: float = 40.0       # 0~255 평균 밝기 임계값
-EDGE_DIFF_THRESHOLD: int = 120          # 에지 강도 임계값
-MIN_EDGE_RATIO: float = 0.02            # 강한 에지 비율 최소치
+MIN_AREA_RATIO: float = 0.005
 
 MAX_TO_SAVE: int = 5                    # 저장할 샘플 수
 
 
-
+# 기본문제 처리
 def resize_to_target(img: Image.Image, size: Tuple[int, int]) -> Image.Image:
     return ImageOps.fit(img.convert("RGB"), size, method=Image.BICUBIC)
 
@@ -32,29 +32,39 @@ def blur(img: Image.Image, radius: float = GAUSSIAN_BLUR_RADIUS) -> Image.Image:
 def hflip(img: Image.Image) -> Image.Image:
     return ImageOps.mirror(img)
 
-# -------------------------------------------------------------------
-# 이상치 필터
-# -------------------------------------------------------------------
+
+# 이상치 
 def is_too_dark(img_gray_u8: Image.Image, mean_thr: float = DARK_MEAN_THRESHOLD) -> bool:
     return np.array(img_gray_u8).mean() < mean_thr
 
 def is_too_small_proxy(img_gray_u8: Image.Image,
-                       edge_thr: int = EDGE_DIFF_THRESHOLD,
-                       min_edge_ratio: float = MIN_EDGE_RATIO) -> bool:
-    arr = np.array(img_gray_u8, dtype=np.int16)
-    gy = np.abs(arr[:, 1:] - arr[:, :-1])
-    gx = np.abs(arr[1:, :] - arr[:-1, :])
-    strong_edges = (gx > edge_thr).sum() + (gy > edge_thr).sum()
-    edge_ratio = strong_edges / (arr.shape[0] * arr.shape[1])
-    return edge_ratio < min_edge_ratio
+                        min_area_ratio: float = MIN_AREA_RATIO) -> bool:
+    # 가장 큰 객체의 면적 / 전체 이미지 면적 < min_area_ratio 이면 '너무 작다'로 판정
+    arr = np.array(img_gray_u8, dtype=np.uint8)
 
+    # 엣지 검출
+    edges = cv2.Canny(arr, 50, 150)
+
+    # 면적 추출
+    cnts, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if not cnts:
+        return True  # 윤곽이 전혀 없으면 '너무 작다/비어있다'로 처리
+
+    # 가장 큰 컨투어 면적 비율
+    h, w = arr.shape[:2]
+    img_area = float(h * w)
+    max_area = max(cv2.contourArea(c) for c in cnts)
+
+    area_ratio = max_area / img_area
+    return area_ratio < min_area_ratio
+
+# 저장
 def save_image(img: Image.Image, path: Path):
     path.parent.mkdir(parents=True, exist_ok=True)
     img.save(path, quality=95)
 
 
 # 메인 실행
-
 def process_and_save():
     
     ds = load_dataset("ethz/food101", split="train")
